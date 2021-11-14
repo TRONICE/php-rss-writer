f5113c3e70fb9659ebd5631a5ba61cac7882d588
johnpupu <john@tronice.com>
2021-11-14 15:00:44 +0800

    fix xml ns for rss

--- src/Suin/RSSWriter/Feed.php
+++ src/Suin/RSSWriter/Feed.php
@@ -39,25 +39,19 @@ class Feed implements FeedInterface
      */
     public function render()
     {
-				$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" />', LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_ERR_FATAL);
-				$xml->addAttribute('xmlns:xmlns:content', 'http://purl.org/rss/1.0/modules/content/'); 
+        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" />', LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_ERR_FATAL);
 
-				foreach ($this->channels as $channel) {
-						$toDom = dom_import_simplexml($xml);
-						$fromDom = dom_import_simplexml($channel->asXML());
-						$toDom->appendChild($toDom->ownerDocument->importNode($fromDom, true));
-				}
+        foreach ($this->channels as $channel) {
+            $toDom = dom_import_simplexml($xml);
+            $fromDom = dom_import_simplexml($channel->asXML());
+            $toDom->appendChild($toDom->ownerDocument->importNode($fromDom, true));
+        }
 
-				// Note that this adds the namespace as an attribute, rather than a declaration
-				if ($this->type == 'rss') {
-						$xml->addAttribute('xmlns:xmlns:atom','http://www.w3.org/2005/Atom'); 
-				} else if ($this->type == 'podcast') {
-						$xml->addAttribute('xmlns:xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd'); 
-						$xml->addAttribute('xmlns:xmlns:googleplay', 'http://www.google.com/schemas/play-podcasts/1.0'); 
-				}
-
-        //reset
-        $this->type = null;
+        // Note that this adds the namespace as an attribute, rather than a declaration
+        if ($this->type == 'podcast') {
+            $xml->addAttribute('xmlns:xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd'); 
+            $xml->addAttribute('xmlns:xmlns:googleplay', 'http://www.google.com/schemas/play-podcasts/1.0'); 
+        }
 
         $dom = new DOMDocument('1.0', 'UTF-8');
         $dom->appendChild($dom->importNode(dom_import_simplexml($xml), true));
